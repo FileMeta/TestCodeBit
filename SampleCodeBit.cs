93899d775f6fc71aa6d40fd92efb4bb5f7ef43c7
Brandt Redd <brandt@redd.org>
2019-01-31 21:45:33 -0700

    Test 1

---
+++ SampleCodeBit.cs
@@ -0,0 +1,10 @@
+﻿/*
+---
+name: SampleCodeBit.cs
+description: SampleCodeBit
+url: https://raw.githubusercontent.com/FileMeta/TestCodeBit/master/SampleCodeBit.cs
+version: 1.0
+keywords: CodeBit
+dateModified: 2019-01-31
+...
+*/
